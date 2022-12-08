6700e8ef95f9525ffe8418ea2491e4a9a7b7a1a3
SidneyBasa <basa206@hotmail.com>
2022-12-08 14:00:33 -0800

    Fixed bug in JavaScript from shapes to topics.

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -3,8 +3,8 @@ var randomTopic = topics[Math.floor(Math.random()* topics.length)];
 
 function listTopics(){
     // var shapes = ["triangle", "square", "pentagon","circle"];
-    for(var x = 0; x < shapes.length; x++){
-        console.log(shapes[x]);
+    for(var x = 0; x < topics.length; x++){
+        console.log(topics[x]);
     }
 }
 
